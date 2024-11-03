fdef5fd9c1b636a266baeb986ebb44bfc7ca61b8
WAWAYU <957506966@qq.com>
2024-11-03 11:43:30 +0800

    改数据库acthub

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 DEBUG = True
 SECRET_KEY = '12345666'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/atm?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/acthub?charset=utf8mb4'
 #SESSION_COOKIE_NAME = ''
 SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
