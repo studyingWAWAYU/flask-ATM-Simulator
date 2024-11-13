1ab1289afc5855c4ac6e860b538ff07602055d9f
wutuu <2753229274@qq.com>
2024-11-13 21:08:27 +0800

    修改了activity.html

--- views/ActivityLobby.py
+++ views/ActivityLobby.py
@@ -2,11 +2,20 @@ from flask import render_template,request,redirect, flash,session
 from flask import Blueprint
 
 from ATMflask import db
-#from ATMflask.sql import User
+from ATMflask.sql import User
 
 actlb = Blueprint('actlb',__name__)
 
 @actlb.route('/ActivityLobby',methods = ['GET','POST'])  # 用装饰器定义路由的对应关系
 def activityLobby():
+    user_id = session.get('id')
+    username = None
+
+    if user_id:
+        user = User.query.get(user_id)
+        username = user.username
+
     if request.method == 'GET':
-        return render_template('ActivityLobby.html')
+        return render_template('ActivityLobby.html',username=username)
+
+# @actlb.route()
