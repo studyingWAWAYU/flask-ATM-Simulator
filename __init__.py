e08e5b4be03100f05835859c75637aef36b8e901
WAWAYU <957506966@qq.com>
2024-11-13 23:19:32 +0800

    新增蓝图

--- __init__.py
+++ __init__.py
@@ -7,12 +7,20 @@ app = Flask(__name__)#实例化flask对象
 app.config.from_object(settings)#配置文件
 db = SQLAlchemy(app)#实例化数据库对象，它提供访问Flask-SQLAlchemy的所有功能
 
+# 配置上传文件目录
+app.config['UPLOAD_FOLDER'] = 'static/uploads/'
 
 from ATMflask.views import index
 app.register_blueprint(index.idx)
 
 from ATMflask.views import loginANDregister
 app.register_blueprint(loginANDregister.lr)
+
+from ATMflask.views import MyActivity
+app.register_blueprint(MyActivity.myAct)
+
+from ATMflask.views import AddActivity
+app.register_blueprint(AddActivity.addAct)
 
 from ATMflask.views import ActivityLobby
 app.register_blueprint(ActivityLobby.actlb)
