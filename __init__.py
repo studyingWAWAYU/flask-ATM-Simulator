96f77526c65526f576d628b24db489eadbce5d38
WAWAYU <957506966@qq.com>
2024-11-14 15:54:19 +0800

    修改

--- __init__.py
+++ __init__.py
@@ -7,9 +7,6 @@ app = Flask(__name__)#实例化flask对象
 app.config.from_object(settings)#配置文件
 db = SQLAlchemy(app)#实例化数据库对象，它提供访问Flask-SQLAlchemy的所有功能
 
-# 配置上传文件目录
-app.config['UPLOAD_FOLDER'] = 'static/img/uploads/'
-
 from ATMflask.views import index
 app.register_blueprint(index.idx)
 
