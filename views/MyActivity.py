cfb7abd1f3d4352d005c04ae8f07be0569384c7c
WAWAYU <957506966@qq.com>
2024-11-15 16:10:46 +0800

    我的活动

--- views/MyActivity.py
+++ views/MyActivity.py
@@ -2,7 +2,7 @@ from flask import render_template,request,redirect, flash,session
 from flask import Blueprint
 
 from ATMflask import db
-from ATMflask.sql import User
+from ATMflask.sql import User,Participant,Activity
 
 myAct = Blueprint('myAct',__name__)
 
@@ -10,10 +10,22 @@ myAct = Blueprint('myAct',__name__)
 def MyActivity():
     user_id = session.get('id')
     username = None
+    myAct = None
 
     if user_id:
         user = User.query.get(user_id)
         username = user.username
 
+        myActId = db.session.query(Participant.activity_id).filter_by(user_id=user_id).all()
+        if myActId == []:
+            flash("You haven't participated in any activity.")
+        else:
+            myActIdLST = [activity_id[0] for activity_id in myActId]  # 把列表嵌套元组改为列表
+            myAct = Activity.query.filter(Activity.activity_id.in_(myActIdLST)).all()
+            print(myAct)
+
+    else:
+        flash("Please login first to check your activities.")
+
     if request.method == 'GET':
-        return render_template('MyActivity.html',username=username)
+        return render_template('MyActivity.html',username=username,myAct=myAct)
