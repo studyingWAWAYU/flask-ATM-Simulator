8981c80595120550686ed108ce18e333b782c551
studyingWAWAYU <131432582+studyingWAWAYU@users.noreply.github.com>
2024-11-29 20:58:26 +0800

    Update ActivityContent.js

--- static/js/ActivityContent.js
+++ static/js/ActivityContent.js
@@ -1,3 +1,21 @@
+function confirmLogout(event) {
+    event.preventDefault();  // 阻止默认行为，即阻止直接跳转
+    const isConfirmed = confirm("Are you sure you want to log out?");
+    if (isConfirmed) {
+        window.location.href = event.target.href;  // 如果确认，执行登出操作
+        document.getElementById("logoutForm").submit();
+    }
+}
+
+function confirmDelete(event){
+    event.preventDefault();
+    console.log("Delete button clicked");
+    var confirmation = confirm("Are you sure to delete this activity? All related data will be removed!")
+    if(confirmation){
+        document.getElementById('deleteForm').submit();
+    }
+}
+
 var idx = 0;
 var t = null;
 var totalImgs = 0;
