04a2eb4798f535b926f5c2f414147c7f854fca94
WAWAYU <957506966@qq.com>
2024-11-30 14:26:54 +0800

    新建MyActivity.js

---
+++ static/js/MyActivity.js
@@ -0,0 +1,8 @@
+function confirmLogout(event) {
+    event.preventDefault();  // 阻止默认行为，即阻止直接跳转
+    const isConfirmed = confirm("Are you sure you want to log out?");
+    if (isConfirmed) {
+        window.location.href = event.target.href;  // 如果确认，执行登出操作
+        document.getElementById("logoutForm").submit();
+    }
+}
