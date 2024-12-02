36268539298941b56e1790ac7953087f64a1249c
Linasdzz <linxi20040107@163.com>
2024-12-03 03:29:55 +0800

    活动页面的修改，报名、管理参与者、发布签到码、签到功能的实现，参与者列表界面的完成

--- __init__.py
+++ __init__.py
@@ -31,14 +31,8 @@ app.register_blueprint(ActivityLobby.actlb)
 from ATMflask.views import ActivityContent
 app.register_blueprint(ActivityContent.actct)
 
-from ATMflask.views import ClubLobby
-app.register_blueprint(ClubLobby.clublb)
-
-from ATMflask.views import ClubDetail
-app.register_blueprint(ClubDetail.clubdt)
-
-from ATMflask.views import MyClub
-app.register_blueprint(MyClub.myclub)
+from ATMflask.views import ActivityManage
+app.register_blueprint(ActivityManage.actManage)
 
 
 
